7c4d9280b04b99c7dc873f64af5fab657b42606f
Chandan kumar Sah <93442672+sahchandan781@users.noreply.github.com>
2024-09-08 21:29:45 +0530

    that is all about hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient"
+import Header from "./components/Header";
+import  Hero  from "./components/Hero";
+
+const App = () => {
 
 
   return (
     <>
-      <h1>hello world</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
+      <ButtonGradient />
     </>
-  )
+  );
 }
 
 export default App
